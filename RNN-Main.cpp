cb70dfb99824d7556be6f8fc0c957e1a15cb2089
Hadi Abdi Khojasteh <khojasteh.programmer@gmail.com>
2017-08-22 10:39:00 +0400

    Make model Ws variables

--- RNN-Main.cpp
+++ RNN-Main.cpp
@@ -33,6 +33,35 @@ int main() {
 		}
 	}
 
+	uint32_t data_size = data.size();
+	uint32_t vocab_size = chars.size();
+	printf("data has %d characters, %d unique.\n", data_size, vocab_size);
+
+	//hyperparameters
+	uint32_t hidden_size = 100;					//size of hidden layer of neurons
+	uint32_t seq_length = 25;					//number of steps to unroll the RNN for
+	uint32_t learning_rate = 1e-1;
+
+	//model parameters
+	Mat1d Wxh(hidden_size, vocab_size);			// Or: Mat mat(2, 4, CV_64FC1);
+	Mat1d Whh(hidden_size, vocab_size);
+	Mat1d Why(hidden_size, vocab_size);
+	double mean = 0.0, stddev = 1.0 / 3.0;		// 99.7% of values will be inside [-1, +1] interval
+
+	randn(Wxh, Scalar(mean), Scalar(stddev));	//input to hidden
+	randn(Whh, Scalar(mean), Scalar(stddev));	//hidden to hidden
+	randn(Why, Scalar(mean), Scalar(stddev));	//hidden to output
+	Mat bh = Mat::zeros(1, hidden_size, CV_32F);//hidden bias
+	Mat by = Mat::zeros(1, vocab_size, CV_32F); //output bias
+
+	uint32_t n, p = 0;
+	//Make an array of zeros with the same shape and type as a Ws array.
+	Mat mWxh = Mat::zeros(Wxh.size(), Wxh.type());
+	Mat mWhh = Mat::zeros(Whh.size(), Whh.type());
+	Mat mWhy = Mat::zeros(Why.size(), Why.type());
+	Mat mbh = Mat::zeros(bh.size(), bh.type());	//memory variables for Adagrad
+	Mat mby = Mat::zeros(by.size(), by.type());	//memory variables for Adagrad
+
 
 	return 0;
 }
